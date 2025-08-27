282fec40bb7447e1efe9b4774f06861504c9ff0d
abdurazakm <abdulkem5472gmail.com>
2025-08-28 01:12:00 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import Navbar from "./components/Navbar";
+// import Home from './pages/Home/Home';
+import ExplorePage from './pages/ExploreIssues/ExplorePage';
+
+
+
+function App() {
+
+  return (
+    <>
+     
+      <div className="App">
+         {/* <Home /> */}
+         <ExplorePage />
+      </div>
+        
+    </>
+  )
+}
+
+export default App
