1b2660a32276d42f5fdd10eef9205a7bc803d55f
abdurazakm <abdulkem5472gmail.com>
2025-11-14 01:12:14 +0300

    cv updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export function Hero({ profileImage }) {
   };
 
   const openResume = () => {
-    window.open('https://drive.google.com/file/d/10Ue5s0OPgBBzZG8IR5UO4K63EQqHq3Zr/view?usp=sharing', '_blank');
+    window.open('https://drive.google.com/file/d/1u-I0UB02DKyJAxHK6h_rUpzlSbw01UUc/view?usp=sharing', '_blank');
   };
 
   return (
