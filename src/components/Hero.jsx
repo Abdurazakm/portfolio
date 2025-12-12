e2c1717c2e200c553dac47c9155c9b012ba5de54
Abdurazakm <162744021+Abdurazakm@users.noreply.github.com>
2025-12-12 17:34:43 +0300

    Update resume link in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export function Hero({ profileImage }) {
   };
 
   const openResume = () => {
-    window.open('https://drive.google.com/file/d/1_2uHnO7f2k_-t6zKBDCCWqpExK_Bk8uy/view?usp=sharing', '_blank');
+    window.open('https://drive.google.com/file/d/1QldGnWmEQ7_HezVMe-EjICNkG4sOXQIC/view?usp=sharing', '_blank');
   };
 
   return (
